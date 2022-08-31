b01907a9764b3cbd949a74fb3100dffcebee3d25
Pricila santos <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-08-31 02:34:21 -0300

    Update main.js
    corrigido classList... faltou a letra maiscula do List

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let contador = 0;
 while(contador < listaDeTeclas.length){
 
     const tecla = listaDeTeclas[contador];   //criado uma constante para receber qual ecla está no loop
-    const instrumento = tecla.classlist[1];  //criado uma constante para receber qual o elemento 1 da lista de classe. no caso a segunda parte do nome do id de audio.
+    const instrumento = tecla.classList[1];  //criado uma constante para receber qual o elemento 1 da lista de classe. no caso a segunda parte do nome do id de audio.
     const idAudio = `#som_${instrumento}`;                         //criado constante para receber um novo id de audio unindo uma parte da string com a segunda parte do nome do id de audio que estaráagora na constante instrumento. usar a crase para poder acessar a função template.
 
     tecla.onclick = function(){
