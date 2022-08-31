c93e37e4ac711ed13524de1848137e6136e4417c
Pricila santos <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-08-31 02:55:44 -0300

    update.. adicionando classe com o JS

--- main copy.js
+++ main copy.js
@@ -5,12 +5,15 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');  // seletor ALL cria uma lista que foi armazenada dentro da constante listaDeTeclas.
 
-for (let contador = 0; contador < listaDeTeclas.length; contador ++){
+while(contador < listaDeTeclas.length){
 
     const tecla = listaDeTeclas[contador];   //criado uma constante para receber qual ecla está no loop
     const instrumento = tecla.classList[1];  //criado uma constante para receber qual o elemento 1 da lista de classe. no caso a segunda parte do nome do id de audio.
     const idAudio = `#som_${instrumento}`;                         //criado constante para receber um novo id de audio unindo uma parte da string com a segunda parte do nome do id de audio que estaráagora na constante instrumento. usar a crase para poder acessar a função template.
 
-    tecla.onclick = function(){tocaSom(idAudio)};
-    tecla.onkeydown = function() {tecla.classList.add('ativa')};
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+            }
+    contador += 1;
+    
 }
