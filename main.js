9452571a3cb111c5daab3b291795c686b92a5e10
Pricila santos <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-08-31 02:23:10 -0300

    Update main.js
    clear cod.

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');  // seletor ALL cria uma lista que foi armazenada dentro da constante listaDeTeclas.
 
+let contador = 0
+
+while(contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classlist[1]
+
+    tecla.onclick = function(){
+        tocaSom()
+            }
+    contador += 1
+    
+}
