fb8ed84704652aa63cc181a4d6923eae4b5df7dc
Pricila santos <106575354+Bruno-MdeMaria@users.noreply.github.com>
2022-08-31 02:12:39 -0300

    armazenando lista

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');  // seletor ALL cria uma lista que foi armazenada dentro da constante listaDeTeclas.
+
