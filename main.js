7baf32e9642a37610b0beaccc7c84515fee116b4
Pricila santos <brunomartinsdemaria@gmail.com>
2022-09-03 07:13:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const listaDeTeclas = document.querySelectorAll('.tecla');  // seletor ALL cria 
 
 for (let contador = 0; contador < listaDeTeclas.length; contador ++){
 
-    const tecla = listaDeTeclas[contador];   //criado uma constante para receber qual ecla está no loop
-    const instrumento = tecla.classList[1];  //criado uma constante para receber qual o elemento 1 da lista de classe. no caso a segunda parte do nome do id de audio.
+    const tecla = listaDeTeclas[contador];   //criado uma constante para receber qual tecla está no loop
+    const instrumento = tecla.classList[1];  //criado uma constante para receber qual o elemento 1 da lista de classe. que no caso é a segunda parte do nome do id de audio.
     const idAudio = `#som_${instrumento}`;                         //criado constante para receber um novo id de audio unindo uma parte da string com a segunda parte do nome do id de audio que estaráagora na constante instrumento. usar a crase para poder acessar a função template.
 
     tecla.onclick = function(){tocaSom(idAudio)};
