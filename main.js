42ea047e1174cbdac7ed8fc1584fd054ace0a55c
Pricila santos <brunomartinsdemaria@gmail.com>
2022-09-03 08:11:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,10 @@ for (let contador = 0; contador < listaDeTeclas.length; contador ++){
 
     const tecla = listaDeTeclas[contador];   //criado uma constante para receber qual tecla está no loop
     const instrumento = tecla.classList[1];  //criado uma constante para receber qual o elemento 1 da lista de classe. que no caso é a segunda parte do nome do id de audio.
-    const idAudio = `#som_${instrumento}`;                         //criado constante para receber um novo id de audio unindo uma parte da string com a segunda parte do nome do id de audio que estaráagora na constante instrumento. usar a crase para poder acessar a função template.
+    const idAudio = `#som_${instrumento}`;                         //criado constante para receber um novo id de audio unindo uma parte da string com a segunda parte do nome do id de audio que estará agora na constante instrumento. usar a crase para poder acessar a função template.
 
     tecla.onclick = function(){tocaSom(idAudio)};
-    tecla.onkeydown = function() {tecla.classList.add('ativa')};
+    tecla.onkeydown = function() {tecla.classList.add('ativa')}; //função ADD adiciona a classe "ativa" que vai deixar a tecla na cor vermelha. o JS passa adicionar a classe ao botão falta remover a classe para apagar o vermelho:
+    tecla.onkeyup = function()[tecla.classList.remove('ativa')]; //função REMOVE remove a classe ativa quer deixa vermelho a tecla. adicioando na variavel função quando a tecla sobe.
+    
 }
